ca1d0f67609fc5588ebd36d24a26f63354bb5ed8
Pramatha <pramatha21992@gmail.com>
2016-04-10 21:36:02 +0100

    final commit

--- adventure.py
+++ adventure.py
@@ -1,4 +1,5 @@
 from data import locations
+from data import rooms
 
 directions = {
     'west': (-1, 0),
@@ -8,18 +9,25 @@ directions = {
 }
 
 position = (0, 0)
-
+valid_directions = {}
+accessories =[]
 while True:
     location = locations[position]
     print("you are at the %s" % location)
+    print("you can choose from these items %s" % rooms[location[0]])
 
-    valid_directions = {}
+    prompt = input("choose the items you need\n")
+    chosen_item = prompt
+    accessories = chosen_item
+    print("your accessories : %s" % accessories)
+
     for k, v in directions.items():
         possible_position = (position[0] + v[0], position[1] + v[1])
         possible_location = locations.get(possible_position)
         if possible_location:
-            print('to the %s is a %s' % (k, possible_location))
+            print('to the %s is a %s' % (k, possible_location[0]))
             valid_directions[k] = possible_position
+
 
     direction = input('which direction do you want to go?\n')
     new_position = valid_directions.get(direction)
--- data.py
+++ data.py
@@ -1,6 +1,12 @@
 locations = {
-    (0, 0): 'house',
-    (0, 1): 'lake',
-    (1, 0): 'park',
-    (1, 1): 'market',
+    (0, 0): ['house', 'H', 'This is a house'],
+    (0, 1): ['lake', 'L', 'This is a lake'],
+    (1, 0): ['park', 'P', 'This is a park'],
+    (1, 1): ['market', 'M', 'This is a market'],
+}
+rooms = {
+    'house': ['Food', 'accessories', 'clothes'],
+    'lake': ['flowers', 'swans', 'ferry'],
+    'park': ['rides', 'plants', 'statues'],
+    'market': ['veggies', 'fruits']
 }
